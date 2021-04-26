d487adca8882b91d7bf51881030f06f89ce83d5b
張晨鈺 <chen-yu@zhangchenyude-MacBook-Pro.local>
2021-04-26 16:08:19 +0800

    add

--- FoodTracker/Controllers/Core Tabs/AddViewController.swift
+++ FoodTracker/Controllers/Core Tabs/AddViewController.swift
@@ -17,23 +17,9 @@ class AddViewController: UIViewController {
     }
     
     
+    
     @IBAction func Scanner(_ sender: Any) {
-        configureDocumentView()
     }
     
-    private func configureDocumentView(){
-        let scanningDocumentVC = VNDocumentCameraViewController()
-        scanningDocumentVC.delegate=self
-        self.present(scanningDocumentVC, animated: true, completion: nil)
-    }
-}
-
-extension AddViewController: VNDocumentCameraViewControllerDelegate{
-    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan){
-        for pageNumber in 0..<scan.pageCount{
-            let image = scan.imageOfPage(at: pageNumber)
-            print(image)
-        }
-        controller.dismiss(animated: true, completion: nil)
-    }
+    
 }
