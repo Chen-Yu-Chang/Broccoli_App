a6fdad28f57560317691197c7fc25704f81234cb
張晨鈺 <chen-yu@zhangchenyude-MacBook-Pro.local>
2021-04-26 13:55:47 +0800

    add

--- FoodTracker/Controllers/Core Tabs/CalendarViewController.swift
+++ FoodTracker/Controllers/Core Tabs/CalendarViewController.swift
@@ -6,24 +6,23 @@
 //
 
 import UIKit
+import FSCalendar
 
-class CalendarViewController: UIViewController {
-
+class CalendarViewController: UIViewController, FSCalendarDelegate {
+    
+    @IBOutlet var calendar: FSCalendar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        calendar.delegate=self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE MM-dd-YYYY"
+        let string = formatter.string(from: date)
+        print("\(string)")
     }
-    */
 
 }
---
+++ FoodTracker/Resources/Calendar.swift
@@ -0,0 +1,13 @@
+//
+//  Calendar.swift
+//  FoodTracker
+//
+//  Created by 張晨鈺 on 2021/4/25.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell{
+    
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
